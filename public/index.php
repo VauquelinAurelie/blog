6ca634a5df7e8490c9d087d4ceb41a43fe819eb1
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-01-24 14:00:03 +0100

    v1.3

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
-Bienvenue sur le blog
+<?php
+$metaTitle = 'Blog';
+
+$routes = [ //lien vers les différentes pages
+    "404" => '../ressources/views/errors/404.php',
+    "toto" => '../ressources/views/errors/404.php',
+];
+
+$p = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+if (array_key_exists($p, $routes)) {
+    ob_start();
+    include $routes[$p];
+    $render = ob_get_clean();
+    echo $render;
+} else {
+    http_response_code(404);
+    include '../ressources/views/errors/404.php';
+    exit;
+}
