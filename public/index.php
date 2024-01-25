dce4db6c2ed2bc1e1a727c06e44875496ba82a41
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-01-25 08:54:08 +0100

    v2.1

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
+<?php include '../config/database.php'; ?>
 <?php
 $metaTitle = 'Blog';
+echo 'Bienvenue sur le blog';
 
 $routes = [ //lien vers les différentes pages
-    "404" => '../ressources/views/errors/404.php',
-    "toto" => '../ressources/views/errors/404.php',
+    //"404" => '../ressources/views/errors/404.php';
+    "home" => '../app/controllers/homeController.php',
 ];
 
 $p = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
+//
 if (array_key_exists($p, $routes)) {
-    ob_start();
+    ob_start(); 
     include $routes[$p];
     $render = ob_get_clean();
     echo $render;
